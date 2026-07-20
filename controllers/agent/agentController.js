707ce1bd7479cd39ad3fe462d421f4898e350b3f
adithyan-sajan <adithyanarayanansajan@gmail.com>
2026-07-20 15:30:46 +0530

    fix:(phase1And2)

--- controllers/agent/agentController.js
+++ controllers/agent/agentController.js
@@ -250,7 +250,7 @@ const agentLoginController = async (req, res, next) => {
     await Promise.all([
       FcmToken.findOneAndUpdate(
         { userId: agentFound._id },
-        { token: fcmToken },
+        { token: [fcmToken] },  // Store as array to match schema [String]
         { upsert: true, new: true }
       ),
       agentFound.save(),
